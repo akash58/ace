dd2af1b6b9075cedc902b296a9769340046bda0f
Akash Vishwakarma <akash.vishwakarma@accionlabs.com>
2020-02-14 08:57:58 +0530

    added pdf

--- src/app/component/modal/modal.component.ts
+++ src/app/component/modal/modal.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 import { Router } from "@angular/router";
+// import {jsPDF} from 'jspdf';
+import * as jsPDF from 'jspdf'
 
 @Component({
   selector: 'app-modal',
@@ -9,6 +11,7 @@ import { Router } from "@angular/router";
 })
 export class ModalComponent implements OnInit {
   @Input() data = { companyName: '', productName: '', rate: 0, cost: 0 };
+  @ViewChild('pdfTable', {static: false}) pdfTable: ElementRef;
   constructor(public activeModal: NgbActiveModal,private router: Router) { }
 
   ngOnInit() {
@@ -17,6 +20,25 @@ export class ModalComponent implements OnInit {
   closeModal() {
     this.activeModal.close("Modal Closed");
     this.router.navigate(["/order-list"]);
+  }
+
+  public downloadAsPDF() {
+    const doc = new jsPDF();
+
+    const specialElementHandlers = {
+      '#editor': function (element, renderer) {
+        return true;
+      }
+    };
+
+    const pdfTable = this.pdfTable.nativeElement;
+
+    doc.fromHTML(pdfTable.innerHTML, 15, 15, {
+      width: 400,
+      'elementHandlers': specialElementHandlers
+    });
+
+    doc.save('PurchaseOrder.pdf');
   }
 
 }
